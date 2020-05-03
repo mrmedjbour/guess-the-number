b3ac6ec113102b6eb0f00f6f816a9c9cf0ad67d5
mrmedjbour <me@medjbour.me>
2020-05-03 19:32:45 +0100

    added image + icon

--- screens/GameOver.js
+++ screens/GameOver.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import {View, Text, Button, StyleSheet, Image} from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 import colors from "../components/colors";
 
 const GameOver = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Is Over!</Text>
-            <Text>Number of Rounds: {props.roundsNumber} </Text>
-            <Text>Number was: {props.userNumber}</Text>
+            <Text style={styles.title}>The Game Is Over <FontAwesome name={'exclamation'} size={18} color={'black'}/> </Text>
+            <View style={styles.imageConatiner}>
+                <Image source={require('../assets/game-over.png')} style={styles.image} resizeMode={'cover'}/>
+            </View>
+            <Text style={styles.textSummry}>Your phone needed <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number <Text style={styles.highlight}>{props.userNumber}</Text></Text>
             <Button onPress={props.onRestart} title={'Start New Game'} color={colors.accent} />
         </View>
     );
@@ -18,6 +21,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 18,
+    },
+    highlight: {
+        color: colors.accent,
+    },
+    textSummry: {
+        fontSize: 15,
+        padding: 15,
+        textAlign: 'center',
+    },
+    imageConatiner: {
+        width: 250,
+        height: 250,
+        borderRadius: 125,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 25,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
     },
 });
 
